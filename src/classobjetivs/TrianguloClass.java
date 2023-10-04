2a4ae19dd018365a11140dce0d32b4eab0367cf9
RenatoCarvalho2022350 <146198748+RenatoCarvalho2022350@users.noreply.github.com>
2023-10-04 13:56:15 +0100

    changing class Triangule

--- src/classobjetivs/TrianguloClass.java
+++ src/classobjetivs/TrianguloClass.java
@@ -10,9 +10,47 @@ package classobjetivs;
  */
 public class TrianguloClass {
     
-    int side1;
-    int side2;
-    int side3;
+    private int side1;
+    private int side2;
+    private int side3;
+
+    public int getSide1() {
+        return side1;
+    }
+
+    public void setSide1(int side1) {
+        if (side1 > 0 ){
+            this.side1 = side1;
+        }else {
+            System.out.println("Cannot set of triangule");
+        }
+    }
+
+    public int getSide2() {
+        return side2;
+    }
+
+    public void setSide2(int side2) {
+        if (side1 > 0 ){
+            this.side2 = side2;
+        }else {
+            System.out.println("Cannot set of triangule");
+        } 
+    }
+
+   
+    public int getSide3() {
+        return side3;
+    }
+
+    public void setSide3(int side3) {
+        if (side1 > 0 ){
+            this.side3 = side3;
+        }else {
+            System.out.println("Cannot set of triangule");
+        }
+    }
+    
     
     
 }
