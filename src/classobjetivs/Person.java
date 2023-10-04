9b9d15312ba77c2914ebd101665de48ff19e6d99
RenatoCarvalho2022350 <146198748+RenatoCarvalho2022350@users.noreply.github.com>
2023-10-04 13:13:54 +0100

    changing class Triangule

---
+++ src/classobjetivs/Person.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classobjetivs;
+
+/**
+ *
+ * @author Ideapad320
+ */
+public class Person {
+    
+    int age;
+    String name;
+    double heigth;
+    boolean IsProgram;
+    TrianguloClass FavTriangulo;
+    
+    
+}
