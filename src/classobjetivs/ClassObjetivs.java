806ac179a47d8a8034c14de54de0ab94385b944d
RenatoCarvalho2022350 <146198748+RenatoCarvalho2022350@users.noreply.github.com>
2023-10-04 12:48:06 +0100

    Update ClassObjetivs.java

--- src/classobjetivs/ClassObjetivs.java
+++ src/classobjetivs/ClassObjetivs.java
@@ -15,6 +15,35 @@ public class ClassObjetivs {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        TrianguloClass tri1 = new TrianguloClass();
+        TrianguloClass tri2 = new TrianguloClass();
+        TrianguloClass tri3 = new TrianguloClass();
+        TrianguloClass tri4 = new TrianguloClass();
+        TrianguloClass tri5 = new TrianguloClass();
+        TrianguloClass tri6 = new TrianguloClass();
+        
+        int[] sideLengt = new int[18];
+        
+        sideLengt[0] = 5;
+        sideLengt[1] = 8; 
+        sideLengt[2] = 9;
+        
+        //tri ( 4,7,1)
+        
+        sideLengt[12] = 4;
+        sideLengt[13] = 7;
+        sideLengt[14] = 2; 
+        
+        
+        //we gonna set up middle of triangule 3
+        sideLengt[1] = 3;
+        sideLengt[4] = 3;
+        sideLengt[7] = 3;
+        sideLengt[10] = 3;
+        sideLengt[12] = 3;
+        sideLengt[16] = 3;
+        
     }
     
 }
